c0a215e19016c90c1d3e7b44167ed4bfb28ad5a5
ike-agu <ikennaww@yahoo.fr>
2025-09-29 01:15:14 +0100

    Create quizData array with sample questions and correct answer in data.js

---
+++ data.js
@@ -0,0 +1,28 @@
+//temporal data for my quiz app
+export const quizData = [
+  {
+    question: "What does DOM stand for?",
+    options: [
+      "Document Order Model",
+      "Document Object Model",
+      "Data Object Model",
+      "Direct Object Model",
+    ],
+    correct: 1,
+  },
+  {
+    question: "Which method selects by ID?",
+    options: [
+      "getElementById()",
+      "querySelectorAll()",
+      "getElement()",
+      "getElementByClassName()",
+    ],
+    correct: 0,
+  },
+  {
+    question: "Which events fires on input change?",
+    options: ["click", "submit", "change", "keydown"],
+    correct: 2,
+  },
+];
---
+++ main.js
@@ -0,0 +1,5 @@
+import { quizData } from "./data.js";
+console.log(quizData)
+
+//create copy of questions and sort them randomly
+let questions = [...quizData]
