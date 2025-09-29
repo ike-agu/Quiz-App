e2fb2934effe93b9b466def7776755320cfd8cfe
ike-agu <ikennaww@yahoo.fr>
2025-09-29 15:34:55 +0100

    Create function to select correct answer; Add styles to option button for correct or wrong answer

--- main.js
+++ main.js
@@ -19,8 +19,31 @@ function loadQuestion(){
     const btn = document.createElement("button")
     btn.classList.add("option-btn") // class for styling the options btn
     btn.textContent = option;
+    btn.addEventListener("click", () => selectAnswer(index))
     options.appendChild(btn)
-  })
+  });
+
+  nextBtn.style.display = "none" // Hides next btn. Only shows when user select a question
 }
+
+
+function selectAnswer(index){
+  const q = questions[currentQuestion];
+  const buttons = document.querySelectorAll(".option-btn")
+
+  buttons.forEach(btn => btn.disabled = true);// once user selects 1 answer, the rest is disabled
+
+  if(index === q.correct){
+    // style the correct answer
+    buttons[index].classList.add("correct");
+  }else{
+    // style the wrong answer
+    buttons[index].classList.add("wrong");
+    //display correct answer again
+    buttons[q.correct].classList.add("correct");
+  }
+  nextBtn.style.display ="inline-block" //next button to display
+}
+
 
 loadQuestion();
